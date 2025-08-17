a4f1eda00d1d3ad3967b76258096f935e726409f
ayush3031 <ayushraj4435@gmail.com>
2025-08-17 15:12:17 +0530

    ready to go first commit

---
+++ backend/phishing-checker/src/main/java/com/cybersec/phishing/checker/PhishingCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.cybersec.phishing.checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhishingCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhishingCheckerApplication.class, args);
+	}
+
+}
---
+++ backend/phishing-checker/src/test/java/com/cybersec/phishing/checker/PhishingCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybersec.phishing.checker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhishingCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
